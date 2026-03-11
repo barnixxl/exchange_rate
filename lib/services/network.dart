493633232cd3baa30baed7a0115ad2ad2b2de8f4
misha <birinmisa15@gmail.com>
2026-03-11 15:26:21 +0300

    add network.dart to work with API

---
+++ lib/services/network.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+
+class Network {
+  late final Dio _dio;
+
+  Network() {
+    _dio = Dio(BaseOptions(
+      baseUrl: 'https://api.exchangerate-api.com/v4/latest/',
+      connectTimeout: Duration(seconds: 10),
+      receiveTimeout: Duration(seconds: 10),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ));
+
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestBody: true,
+      responseBody: true,
+      error: true,
+    ));
+  }
+
+  Dio get dio => _dio;
+
+  Future<Response<T>> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) {
+    return _dio.get<T>(path, queryParameters: queryParameters);
+  }
+}
