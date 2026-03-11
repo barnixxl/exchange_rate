73b979facb37186829e9f7c5b5a2afbefbbb79cb
misha <birinmisa15@gmail.com>
2026-03-11 15:56:35 +0300

    add detail_controller to connect UI with services

---
+++ lib/controllers/detail_controller.dart
@@ -0,0 +1,39 @@
+import 'package:mobx/mobx.dart';
+import '../models/currency_model.dart';
+
+class DetailController {
+  DetailController(this._currency);
+
+  final CurrencyModel _currency;
+
+  String get code => _currency.code;
+  String get name => _currency.name;
+  double get rate => _currency.rate;
+  DateTime get date => _currency.date;
+
+  String formatDate() {
+    return '${date.day} ${_getMonthName(date.month)} ${date.year}, ${date.hour}:${date.minute.toString().padLeft(2, '0')}';
+  }
+
+  String calculate(int amount) {
+    return (amount * _currency.rate).toStringAsFixed(2);
+  }
+
+  String _getMonthName(int month) {
+    const months = [
+      'января',
+      'февраля',
+      'марта',
+      'апреля',
+      'мая',
+      'июня',
+      'июля',
+      'августа',
+      'сентября',
+      'октября',
+      'ноября',
+      'декабря'
+    ];
+    return months[month - 1];
+  }
+}
