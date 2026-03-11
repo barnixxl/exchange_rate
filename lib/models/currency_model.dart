46e6bd2b4a14c0d2cbd73eae43f6f2ef45a5ffea
misha <birinmisa15@gmail.com>
2026-03-11 15:42:01 +0300

    refresh currency_model

--- lib/models/currency_model.dart
+++ lib/models/currency_model.dart
@@ -1,8 +1,26 @@
+import '../services/currency_response.dart';
+
 class CurrencyModel {
   final String code;
   final String name;
   final double rate;
   final DateTime date;
+
+  static const List<String> supportedCurrencies = [
+    'USD',
+    'EUR',
+    'GBP',
+    'CNY',
+    'JPY',
+  ];
+
+  static const Map<String, String> _currencyNames = {
+    'USD': 'Доллар США',
+    'EUR': 'Евро',
+    'GBP': 'Фунт стерлингов',
+    'JPY': 'Японская иена',
+    'CNY': 'Китайский юань',
+  };
 
   CurrencyModel({
     required this.code,
@@ -11,7 +29,16 @@ class CurrencyModel {
     required this.date,
   });
 
-// использую фабричный метод для облегчения приложения
+  factory CurrencyModel.fromResponse(
+      CurrencyApiResponse response, String code) {
+    final rate = response.rates[code]?.toDouble() ?? 0.0;
+    return CurrencyModel(
+      code: code,
+      name: _currencyNames[code] ?? code,
+      rate: rate,
+      date: DateTime.parse(response.date),
+    );
+  }
 
   factory CurrencyModel.fromJson(Map<String, dynamic> json) {
     return CurrencyModel(
