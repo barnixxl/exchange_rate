467171147b878f6dbe16a5e2939aca05eeefc847
misha <birinmisa15@gmail.com>
2026-04-02 17:42:21 +0300

    add block try/catch to api's status

--- lib/network/currency/currency_api.dart
+++ lib/network/currency/currency_api.dart
@@ -10,23 +10,30 @@ class CurrencyApi {
 
   Future<CurrencyResult<List<RateDataFromNetwork>>> fetchRates() async {
     const url = '/rates?periodicity=0';
-    final response = await _network.get(url);
-    if (response.statusCode == 200) {
-      final data = response.data as List<dynamic>?;
-      if (data != null) {
-        final rates = data
-            .map((e) => RateDataFromNetwork.fromJson(e as Map<String, dynamic>))
-            .toList();
-        return CurrencyResult.success(rates);
+
+    try {
+      final response = await _network.get(url);
+
+      if (response.statusCode == 200) {
+        final List<dynamic>? ratesData = response.data;
+        if (ratesData != null) {
+          final rates = ratesData
+              .map((e) =>
+              RateDataFromNetwork.fromJson(e as Map<String, dynamic>))
+              .toList();
+          return CurrencyResult.success(rates);
+        }
+      } else {
+        return CurrencyResult.failure(
+          CurrencyError(
+            code: 'LOAD_FAILED',
+            message: response.statusMessage ?? 'Ошибка загрузки',
+          ),
+        );
       }
-    } else {
-      return CurrencyResult.failure(
-        CurrencyError(
-          code: 'LOAD_FAILED',
-          message: response.statusMessage ?? 'Ошибка загрузки',
-        ),
-      );
+      return CurrencyResult.failure(CurrencyError.parsing());
+    } catch (e) {
+      return CurrencyResult.failure(CurrencyError.fromException(e));
     }
-    return CurrencyResult.failure(CurrencyError.parsing());
   }
 }
