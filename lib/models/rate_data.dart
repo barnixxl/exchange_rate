7bc21e43789f21a501c41fc17f1aa39900e2897f
misha <birinmisa15@gmail.com>
2026-04-07 11:39:15 +0300

    change final-chars and remove double method

--- lib/models/rate_data.dart
+++ lib/models/rate_data.dart
@@ -24,26 +24,11 @@ class RateData {
     required this.date,
   });
 
-  factory RateData.fromRateData(RateDataFromNetwork rateData) {
-    final code = rateData.curAbbreviation ?? '';
-    final name = rateData.curName ?? 'Неизвестное название валюты';
-    final scale = rateData.curScale ?? 1;
-    final officialRate = rateData.curOfficialRate ?? 0.0;
-    final dateStr = rateData.date ?? DateTime.now().toIso8601String();
-    return RateData(
-      code: code,
-      name: name,
-      rate: officialRate / scale,
-      date: DateTime.parse(dateStr),
-    );
-  }
-
   static RateData fromResponse(List<RateDataFromNetwork> rates, String code) {
     final rateData = rates.firstWhere((r) => r.curAbbreviation == code,
         orElse: () => throw CurrencyError(
-            code: 'CURRENCY_NOT_FOUND', message: 'Валюта $code не найдена')
-    );
-    return RateData.fromRateData(rateData);
+            code: 'CURRENCY_NOT_FOUND', message: 'Валюта $code не найдена'));
+    return RateData.fromNetworkModel(rateData);
   }
 
   factory RateData.fromJson(Map<String, dynamic> json) {
@@ -51,8 +36,7 @@ class RateData {
       code: json['code'] ?? '',
       name: json['name'] ?? 'данная валюта отсутствует',
       rate: (json['rate'] ?? 0.0).toDouble(),
-      date: DateTime.parse(json['date'] ?? DateTime.now().toIso8601String()
-      ),
+      date: DateTime.parse(json['date'] ?? DateTime.now().toIso8601String()),
     );
   }
 
@@ -72,17 +56,12 @@ class RateData {
     );
   }
 
-  static RateData fromNetworkModel(RateDataFromNetwork? model){
-    final code = model?.curAbbreviation ?? '';
-    final name = model?.curName ?? 'Неизвестное название валюты';
-    final scale = model?.curScale ?? 1;
-    final officialRate = model?.curOfficialRate ?? 0.0;
-    final dateStr = model?.date ?? DateTime.now().toIso8601String();
+  static RateData fromNetworkModel(RateDataFromNetwork model) {
     return RateData(
-      code: code,
-      name: name,
-      rate: officialRate / scale,
-      date: DateTime.parse(dateStr),
+      code: model.curAbbreviation ?? '',
+      name: model.curName ?? 'Неизвестное название валюты',
+      rate: (model.curOfficialRate ?? 0.0) / (model.curScale ?? 1),
+      date: DateTime.parse(model.date ?? DateTime.now().toIso8601String()),
     );
   }
 
