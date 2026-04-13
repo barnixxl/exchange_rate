2e85492b78a6071d5dc5e514fd9e7c5a60ff0847
misha <birinmisa15@gmail.com>
2026-04-13 18:31:25 +0300

    make static final to DateFormat

--- lib/utils/date_formatter.dart
+++ lib/utils/date_formatter.dart
@@ -1,13 +1,11 @@
 import 'package:intl/intl.dart';
 
-class DateFormatter {
-  static final DateFormat dayMonthYearTextDateFormat =
-  DateFormat('d MMMM y, HH:mm', 'ru');
-}
 extension DateTimeUtils on DateTime? {
+  static final DateFormat _formatter = DateFormat('d MMMM y, HH:mm', 'ru');
+
   String? toDayMonthYearTextDateFormat() {
     if (this != null) {
-      return DateFormatter.dayMonthYearTextDateFormat.format(this!);
+      return _formatter.format(this!);
     }
     return null;
   }
