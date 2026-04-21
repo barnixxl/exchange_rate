93a1add5a279d5e8e15cdf87631201e60d10913b
misha <birinmisa15@gmail.com>
2026-04-21 19:43:43 +0300

    fix currency's filtration

--- lib/services/currency_repository.dart
+++ lib/services/currency_repository.dart
@@ -3,7 +3,7 @@ import '../models/currency_result.dart';
 import '../network/currency/currency_api.dart';
 
 class CurrencyRepository {
-  static const _targerCurrencies = ["USD", "EUR", "CNY", "PLN", "UAH"];
+  static const _targetCurrencies = ["USD", "EUR", "CNY", "PLN", "UAH"];
 
   final CurrencyApi _api;
 
@@ -11,17 +11,21 @@ class CurrencyRepository {
 
   Future<CurrencyResult<List<RateData>>> fetchRates() async {
     final result = await _api.fetchRates();
-    if(result.isSuccess) {
-      final filtered = result.data!
-          .where((r) => _targerCurrencies.contains(r.code))
-          .toList()
-          ..sort((a, b){
-            final indexA = _targerCurrencies.indexOf(a.code);
-            final indexB = _targerCurrencies.indexOf(b.code);
-            return indexA.compareTo(indexB);
-          });
-      return CurrencyResult.success(filtered);
+
+    if (result.isSuccess) {
+      _filterAndSortRates(result.data);
     }
     return result;
+  }
+
+  void _filterAndSortRates(List<RateData>? rates) {
+    if (rates == null) return;
+    rates
+      ..retainWhere((rate) => _targetCurrencies.contains(rate.code))
+      ..sort((a, b) {
+        final indexA = _targetCurrencies.indexOf(a.code);
+        final indexB = _targetCurrencies.indexOf(b.code);
+        return indexA.compareTo(indexB);
+      });
   }
 }
