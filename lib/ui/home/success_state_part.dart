614a35a9cb6b95077d0e83ae28520866b22c5468
misha <birinmisa15@gmail.com>
2026-04-24 17:33:11 +0300

    Revert "use parts in home_screen and fix bug in the internet connection manifest"
    This reverts commit 94d0f2efee7e38b017f445bbae7c2919fd7f7e82.

--- lib/ui/home/success_state_part.dart
+++
@@ -1,8 +0,0 @@
-part of 'home_screen.dart';
-
-extension _HomeScreenSuccessStatePart on _HomeScreenState {
-  Widget _buildSuccessState(List<RateData>? data) {
-    final currencies = data ?? <RateData>[];
-    return _buildCurrencyList(currencies);
-  }
-}
