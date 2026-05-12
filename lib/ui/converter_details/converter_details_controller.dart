bb9db37810bba6bf32319d093ff964e8429c162b
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-05-12 15:21:41 +0300

    fix bugs

--- lib/ui/converter_details/converter_details_controller.dart
+++ lib/ui/converter_details/converter_details_controller.dart
@@ -1,80 +1,54 @@
-import 'package:currency_converter/models/currency_result.dart';
 import 'package:mobx/mobx.dart';
 
+import '../../main.dart';
+import '../../models/currency_result.dart';
 import '../../models/rate_data.dart';
 import '../../utils/date_formatter.dart';
-import '../../main.dart';
 
 class ConverterDetailsController {
   final Observable<CurrencyResult<RateData>> _currencyResult = Observable(
     CurrencyResult.notInitialized(),
   );
-
   final Observable<String> baseAmountInput = Observable('');
   final Observable<String> currencyAmountInput = Observable('');
 
   String get code => _currencyResult.value.data?.code ?? '';
-
   String get name => _currencyResult.value.data?.name ?? '';
-
   double get rate => _currencyResult.value.data?.rate ?? 0.0;
-
   int get scale => _currencyResult.value.data?.scale ?? 1;
-
   DateTime? get date => _currencyResult.value.data?.date;
-
   String? get formattedDate => date?.toDayMonthYearTextDateFormat();
-
   String get exchangeRateText => strings.common_scale_equals_rate_byn(
         scale,
         code,
         rate.toStringAsFixed(4),
       );
-
   String get updatedDateText => formattedDate ?? strings.common_absent_date;
-
   bool get hasBaseAmount => baseAmountInput.value.isNotEmpty;
-
   bool get hasCurrencyAmount => currencyAmountInput.value.isNotEmpty;
-
-  String get convertedAmount {
-    final amount = _parseAmount(baseAmountInput.value);
-    return _calculateForward(amount);
-  }
-
-  String get convertedAmountReverse {
-    final amount = _parseAmount(currencyAmountInput.value);
-    return _calculateReverse(amount);
-  }
+  String get convertedAmount => _calculateForward(
+        _parseAmount(baseAmountInput.value),
+      );
+  String get convertedAmountReverse => _calculateReverse(
+        _parseAmount(currencyAmountInput.value),
+      );
 
   void onBaseAmountChanged(String value) {
     runInAction(() {
-      _onBaseAmountChanged(value);
+      baseAmountInput.value = value;
     });
   }
 
   void onCurrencyAmountChanged(String value) {
     runInAction(() {
-      _onCurrencyAmountChanged(value);
+      currencyAmountInput.value = value;
     });
   }
 
   void loadCurrency(RateData currency) {
     runInAction(() {
-      _loadCurrency(currency);
+      _currencyResult.value = CurrencyResult.success(currency);
     });
-  }
-
-  void _onBaseAmountChanged(String value) {
-    baseAmountInput.value = value;
-  }
-
-  void _onCurrencyAmountChanged(String value) {
-    currencyAmountInput.value = value;
-  }
-
-  void _loadCurrency(RateData currency) {
-    _currencyResult.value = CurrencyResult.success(currency);
   }
 
   double _parseAmount(String input) => double.tryParse(input) ?? 0.0;
