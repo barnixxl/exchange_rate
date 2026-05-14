4122469c21459363f2352fdf4fa842d8e3bd126d
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-05-14 21:41:14 +0300

    add images like assets and make app_images.dart with base images size 1.0x

---
+++ lib/resources/images/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String usd = 'assets/images/usd.png';
+  static const String eur = 'assets/images/eur.png';
+  static const String cny = 'assets/images/cny.png';
+  static const String pln = 'assets/images/pln.png';
+  static const String uah = 'assets/images/uah.png';
+}
