c52ff3875d4fd35c3175cfa83bf8f6b3529088a6
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-05-14 18:31:42 +0300

    remove commas in colors

--- lib/resources/colors/app_colors.dart
+++ lib/resources/colors/app_colors.dart
@@ -2,14 +2,11 @@ import 'dart:ui';
 
 class AppColors {
   static const roseBrand = Color(0xFFFF3689);
-
   static const primary = Color(0xFF2196F3);
   static const primaryLight = Color(0xFFBBDEFB);
   static const primaryDark = Color(0xFF1976D2);
-
   static const onPrimary = Color(0xFFFFFFFF);
   static const onPrimarySubtle = Color(0xB3FFFFFF);
-
   static const textSecondary = Color(0xFF9E9E9E);
   static const error = Color(0xFFF44336);
 }
