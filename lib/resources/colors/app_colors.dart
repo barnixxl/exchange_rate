51212d3773a74911c23f3991800329fb8f4f0b63
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-05-20 10:08:17 +0300

    change colors

--- lib/resources/colors/app_colors.dart
+++ lib/resources/colors/app_colors.dart
@@ -1,12 +1,11 @@
 import 'dart:ui';
 
 class AppColors {
-  static const roseBrand = Color(0xFFFF3689);
   static const primary = Color(0xFF5A1919);
-  static const primaryLight = Color(0xFF9C434F);
+  static const primaryLight = Color(0xFFB5868A);
   static const primaryDark = Color(0xFF703C44);
   static const onPrimary = Color(0xFFFFFFFF);
   static const onPrimarySubtle = Color(0xB3FFFFFF);
-  static const textSecondary = Color(0xFF9E9E9E);
   static const error = Color(0xFFF44336);
+  static const shadow = Color(0xFF8C5B5F);
 }
